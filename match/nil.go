2f8418bcd15d5dc996906c81ca151e22579321f3
Andy Kreling <krelinga@gmail.com>
2025-11-07 06:42:23 +0000

    Nil().

---
+++ match/nil.go
@@ -0,0 +1,24 @@
+package match
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/krelinga/go-deep"
+)
+
+func Nil() Matcher {
+	return Func(func(env deep.Env, vals Vals) Result {
+		got := vals.Want1()
+		switch got.Kind() {
+		case reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Pointer, reflect.Slice:
+			// Expected case
+		default:
+			panic(fmt.Errorf("%w: value does not support the Nil operation: %s", ErrBadType, got.Kind()))
+		}
+		if got.IsNil() {
+			return NewResult(true, "value is nil")
+		}
+		return NewResult(false, "value is not nil")
+	})
+}
