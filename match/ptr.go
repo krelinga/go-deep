93ded05cc574a779da8d31cc0699ae544a31f606
Andy Kreling <krelinga@gmail.com>
2025-11-07 06:45:42 +0000

    Pointer().

---
+++ match/ptr.go
@@ -0,0 +1,22 @@
+package match
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/krelinga/go-deep"
+)
+
+func Pointer(m Matcher) Matcher {
+	return Func(func(env deep.Env, vals Vals) Result {
+		got := vals.Want1()
+		if got.Kind() != reflect.Ptr {
+			panic(fmt.Errorf("%w: value is not a pointer: got %s", ErrBadType, got.Kind()))
+		}
+		if got.IsNil() {
+			return NewResult(false, "pointer is nil")
+		}
+		elem := got.Elem()
+		return m.Match(env, Vals{elem})
+	})
+}
