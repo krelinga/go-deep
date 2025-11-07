2c12f3b222e7fb2428e6ed811543306bc6d1e99e
Andy Kreling <krelinga@gmail.com>
2025-11-07 07:24:20 +0000

    Struct{}.

---
+++ match/struct.go
@@ -0,0 +1,30 @@
+package match
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/krelinga/go-deep"
+)
+
+type Struct map[string]Matcher
+
+func (s Struct) Match(env deep.Env, vals Vals) Result {
+	got := vals.Want1()
+	if got.Kind() != reflect.Struct {
+		panic(fmt.Errorf("%w: value is not a struct: got %s", ErrBadType, got.Kind()))
+	}
+	for fieldName, matcher := range s {
+		t := got.Type()
+		field, ok := t.FieldByName(fieldName)
+		if !ok {
+			panic(fmt.Errorf("%w: struct does not have field %q", ErrBadType, fieldName))
+		}
+		fieldVal := got.FieldByIndex(field.Index)
+		result := matcher.Match(env, Vals{fieldVal})
+		if !result.Matched() {
+			return NewResult(false, fmt.Sprintf("field %q does not match", fieldName))
+		}
+	}
+	return NewResult(true, "struct matches")
+}
