c115b6122672fb942f28c37579b2970c48691572
Andy Kreling <krelinga@gmail.com>
2025-11-07 07:47:49 +0000

    Split up Match() and add MatchVals().

--- deeptest/match.go
+++ deeptest/match.go
@@ -3,7 +3,13 @@ package deeptest
 import "github.com/krelinga/go-deep/match"
 
 func Match[T any](env Env, got T, m match.Matcher) Result {
+	env.Helper()
 	vals := match.NewVals1(got)
+	return MatchVals(env, vals, m)
+}
+
+func MatchVals(env Env, vals match.Vals, m match.Matcher) Result {
+	env.Helper()
 	r := m.Match(env, vals)
 	if !r.Matched() {
 		env.Errorf("Match failed:\n%s", r)
