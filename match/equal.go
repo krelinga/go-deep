bd758d39927145226d15b6d4e19d92dcd60a1693
Andy Kreling <krelinga@gmail.com>
2025-11-07 05:22:06 +0000

    equal.go.

---
+++ match/equal.go
@@ -0,0 +1,23 @@
+package match
+
+import "github.com/krelinga/go-deep"
+
+func Equal[T any](want T) Matcher {
+	return Func(func(env deep.Env, vals Vals) Result {
+		got := Want1[T](vals)
+		if deep.Equal(env, got, want) {
+			return NewResult(true, "values are equal")
+		}
+		return NewResult(false, "values are not equal")
+	})
+}
+
+func NotEqual[T any](want T) Matcher {
+	return Func(func(env deep.Env, vals Vals) Result {
+		got := Want1[T](vals)
+		if !deep.Equal(env, got, want) {
+			return NewResult(true, "values are not equal")
+		}
+		return NewResult(false, "values are equal")
+	})
+}
