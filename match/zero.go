76e6f660de4b883859a4b0938850a3c4832b02fd
Andy Kreling <krelinga@gmail.com>
2025-11-11 03:34:39 +0000

    Zero() matcher.

---
+++ match/zero.go
@@ -0,0 +1,13 @@
+package match
+
+import "github.com/krelinga/go-deep"
+
+func Zero() Matcher {
+	return Func(func(env deep.Env, vals Vals) Result {
+		val := vals.Want1()
+		if val.IsZero() {
+			return NewResult(true, "Is zero value")
+		}
+		return NewResult(false, "Is not zero value")
+	})
+}
