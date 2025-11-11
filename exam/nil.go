8b84913cf844848ab65138b7f902894645c6f313
Andy Kreling <krelinga@gmail.com>
2025-11-11 06:09:59 +0000

    exam.Nil() and exam.NotNil().

---
+++ exam/nil.go
@@ -0,0 +1,18 @@
+package exam
+
+import (
+	"github.com/krelinga/go-deep"
+	"github.com/krelinga/go-deep/match"
+)
+
+func Nil[Type any](e E, env deep.Env, a Type) Result {
+	e.Helper()
+	m := match.Nil()
+	return Match(e, env, a, m)
+}
+
+func NotNil[Type any](e E, env deep.Env, a Type) Result {
+	e.Helper()
+	m := match.Not(match.Nil())
+	return Match(e, env, a, m)
+}
